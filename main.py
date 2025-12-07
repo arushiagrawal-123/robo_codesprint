8c044ecb2abad33d80e5c1447cb049abe4f6370f
arushiagrawal-123 <agrawalarushi123@gmail.com>
2025-12-08 03:46:26 +0530

    update main.py to show automated replies

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import json
 from youtube_fetch import fetch_comments
 from aggregator import aggregate
+from extras import auto_reply
+from sentiment_module import classify_sentiment
 
-# ---- Fetch comments ----
+#Fetch comments
 api = "AIzaSyCXGGghh2Tmgn_6ljFHevnJvlo-WVpxxZM"
-video = "YQHsXMglC9A"   # extracted ID
+video = "YQHsXMglC9A"
 
 comments = fetch_comments(api, video, 60)
 
+# Add test toxic comment BEFORE aggregation
+comments.append({"text": "You are stupid and this is trash!", "likeCount": 0})
+
 result = aggregate(comments)
 
-# ---- Output ----
+#Output
 print("\nSENTIMENT SUMMARY")
 print(result["sentiment_counts"])
 print(result["sentiment_percent"])
@@ -18,7 +23,6 @@ print(result["sentiment_percent"])
 print("\nCOMMENT TYPE BREAKDOWN")
 print(result["category_counts"])
 
-comments.append({"text": "You are stupid and this is trash!", "likeCount": 0}) # test toxic comment
 print("\nTOXIC COMMENTS DETECTED")
 for c in result["toxic_comments"]:
     print(f"- {c['text']} (Likes: {c['likeCount']})")
@@ -29,3 +33,10 @@ print(result["keywords"])
 print("\nTOP COMMENTS")
 for c in result["top_comments"]:
     print(f"- {c['text']} (Likes: {c['likeCount']})")
+
+print("\nAUTO-REPLIES")
+for c in comments:
+    sentiment = classify_sentiment(c["text"])["label"]
+    reply = auto_reply(c["text"], sentiment)
+    print(f"Comment: {c['text']}")
+    print(f"Reply → {reply}\n")
